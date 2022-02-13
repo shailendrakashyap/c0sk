4b0f588282c94ea92681978b360b23a299b8ed64
Shailendra Kashyap <46565698+shailendra0s@users.noreply.github.com>
2022-02-13 13:37:37 +0530

    Create swap.c
    Version 1.0
    Base structure of c program to swap, with variables already taken.

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+int x;
+int y;
+
+printf("Enter x value to swap for y : \n");
+scanf("%d" ,&x);
+
+printf("Enter y value to swap for x : \n");
+scanf("%d" ,&y);
+
+// Swapping...
+
+printf("x is now %d \n" ,x);
+printf("y is now %d \n" ,y);
+}
